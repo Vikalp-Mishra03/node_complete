c65a5f57e59b05a6eeeb3220c9476d7202dbb9e0
Vikalp Mishra <mishravikalp896@gmail.com>
2023-06-27 15:06:39 +0530

    import export of module

--- day 3/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-
-// console.log(path.dirname('C:/Users/Vikalp Mishra/Desktop/complete node in a week/day 3/index.js'))
-// console.log(path.extname('C:/Users/Vikalp Mishra/Desktop/complete node in a week/day 3'))
-// console.log(path.basename('C:/Users/Vikalp Mishra/Desktop/complete node in a week/day 3/index.js'))
-
-// // it give all the above data in single function
-// console.log(path.parse('C:/Users/Vikalp Mishra/Desktop/complete node in a week/day 3/index.js'))
