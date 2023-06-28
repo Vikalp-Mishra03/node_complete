8521b715c8e3518c8534f81d20664bb7f42078f7
Vikalp Mishra <mishravikalp896@gmail.com>
2023-06-28 20:26:21 +0530

    Conversion of json to object and Object to json

--- day 4/JSON/index.js
+++ day 4/JSON/index.js
@@ -2,3 +2,17 @@
 
 // JSON is often used when data is sent from a server to a web page
 
+const bioData = {
+    name: 'Vikalp',
+    age: 26,
+    channel: 'Vikalp',
+};
+
+
+// object ko jab JSON mai create krna hai tab stringify function ka use karenge 
+const jsonData = JSON.stringify(bioData);
+console.log(jsonData)
+
+// JSON ko object mai convert krne ke liye parse function ka use karenge
+const objData = JSON.parse(jsonData)
+console.log(objData)
