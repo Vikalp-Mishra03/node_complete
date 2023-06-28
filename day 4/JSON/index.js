de9d9e8c959fa3a020a1a0761d25b77c1073f480
Vikalp Mishra <mishravikalp896@gmail.com>
2023-06-28 20:11:58 +0530

    adding index file in JSON '

---
+++ day 4/JSON/index.js
@@ -0,0 +1,4 @@
+// JSON stand for Javascript Object Notation. JSON is a light weight format for starting and transporting data
+
+// JSON is often used when data is sent from a server to a web page
+
