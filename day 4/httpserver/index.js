89243b8b0c99ead32a614a3bfa381835bbf2491f
Vikalp Mishra <mishravikalp896@gmail.com>
2023-06-28 18:31:08 +0530

    adding day

---
+++ day 4/httpserver/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// to create a web server in node.js we must use a method http.createServer()
+
+// the request object can be used to get information about the current HTTP request eg. url, request header, and data
+const server = http.createServer((req, res) => {
+    res.end('Hello from the other side');
+});
+
+server.listen(80000, '127.0.0.1', () => {
+    console.log('listening to the port no. 03');
+});
